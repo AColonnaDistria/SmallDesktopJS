ae3ac687b95c63133c72fa3a56e02552e88345f3
AColonnadistria <antoine.colonnadistria@yahoo.com>
2025-01-29 02:40:21 +0100

    Added workspace support

--- window.js
+++ window.js
@@ -1,10 +1,22 @@
-var allWindows = []; // by order
+var currentWorkspace = 0;
+var numberOfWorkspaces = 3;
+
+var allWindows = []; // by workspace, by order
+initWindowsStack();
 
 var window1 = createWindow("Hello", "#181818")
 var window2 = createWindow("World", "#181818");
 var window3 = createWindow("Dark", "#181818");
+var window4 = createWindow("Workspace2", "red", 1);
 
 resetOrder();
+
+function initWindowsStack() {
+    for (let workspace = 0; workspace < numberOfWorkspaces; ++workspace) {
+        allWindows.push([])
+        createWorkspaceIndicator(workspace);
+    }
+}
 
 function setWindowEvent(window) {
     let state = "UP";
@@ -43,7 +55,7 @@ function setWindowEvent(window) {
     });
 }
 
-function createWindow(title, color) {
+function createWindow(title, color, workspace = currentWorkspace) {
     let window = document.createElement("div");
     window.classList.add("window");
 
@@ -53,26 +65,63 @@ function createWindow(title, color) {
 
     window.appendChild(windowHeaderBar);
     window.style.backgroundColor = color;
+
+    if (workspace != currentWorkspace) {
+        hideWindow(window);
+    }
     
     setWindowEvent(window);
 
     document.body.appendChild(window);
-    allWindows.push(window);
+    allWindows[workspace].push(window);
     
     return window;
 }
 
-function setFocus(window) {
-    allWindows.splice(allWindows.indexOf(window), 1);
-    allWindows.push(window);
+function createWorkspaceIndicator(number) {
+    let workspaceIndicator = document.createElement("button");
+    workspaceIndicator.classList.add("workspace-indicator");
+    workspaceIndicator.textContent = `${number + 1}`;
 
-    console.log(allWindows);
+    workspaceIndicator.addEventListener("click", () => {
+        console.log(number);
+        changeWorkspace(number);
+    });
+
+    document.getElementsByClassName("workspace-indicators-group")[0].appendChild(workspaceIndicator);
+
+    return workspaceIndicator;
+}
+
+function hideWindow(window) {
+    window.style.visibility = "hidden";
+}
+
+function showWindow(window) {
+    window.style.visibility = "visible";
+}
+
+function changeWorkspace(newWorkspace) {
+    for (let window of allWindows[currentWorkspace]) {
+        hideWindow(window);
+    }
+    for (let window of allWindows[newWorkspace]) {
+        showWindow(window);
+    }
+    currentWorkspace = newWorkspace;
+}
+
+function setFocus(window) {
+    allWindows[currentWorkspace].splice(allWindows[currentWorkspace].indexOf(window), 1);
+    allWindows[currentWorkspace].push(window);
+
+    console.log(allWindows[currentWorkspace]);
 
     resetOrder();
 }
 
 function resetOrder() {
-    for (let i = 0; i < allWindows.length; ++i) {
-        allWindows[i].style.zIndex = `${100 + i}`
+    for (let i = 0; i < allWindows[currentWorkspace].length; ++i) {
+        allWindows[currentWorkspace][i].style.zIndex = `${100 + i}`
     }
 }
